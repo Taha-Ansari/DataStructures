7b778c940ffe5deb998ed8a63c77ba67d9ebf237
Taha <Taha-Ansari@hotmail.com>
2018-10-21 21:12:40 -0400

    insert to back implemented

--- LinkedList_C/linkedList.c
+++ LinkedList_C/linkedList.c
@@ -1,5 +1,70 @@
 #include "linkedList.h"
 
+//example usage for linked list
 int main(){
-    printf("YAH YEET\n");
+
+    //Creating head node to start the linked list
+    node * head = createHeadNode(6);
+    
+    //inserting to list sequentially (insert at the end)
+    printf("\nINSERTING 6,7,8,9,10 TO THE BACK OF THE LIST\n");
+
+    for(int i=7; i<11; i++){
+        insertToBack(head, i);
+    }
+    printList(head);
+    printf("\nINSERTING 1,2,3,4,5 TO HEAD OF THE LIST\n");
+
+    for(int k=5; k>0; k--){
+        insertToFront(head, k);
+    }
+    printList(head);
+
+    return(0);
+}
+
+node * createHeadNode(int id){
+    node * newNode = NULL;
+    newNode = malloc(sizeof(node));
+    newNode->id = id;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void insertToFront(node * head, int value){
+    node * newNode = malloc(sizeof(node));
+    //creating new node to insert to the head
+    newNode->id = value;
+    newNode->next = temp;
+
+
+    newNode = head;
+}
+
+
+
+//function to insert to the back of the list 
+void insertToBack(node * head, int value){
+    node * newNode = malloc(sizeof(node));
+    node * currentNode = head;
+    //iterating through list until end of list found
+    while(currentNode->next != NULL){
+        currentNode = currentNode->next;
+    }
+    //creating new node to insert to the end
+    newNode->id = value;
+    newNode->next = NULL;
+    //insert new node to the end of the list
+    currentNode->next = newNode;    
+}
+
+void printList(node * head){
+    node * list = head;   
+    int counter = 0;
+    printf("Current list: \n");
+    while(list != NULL){
+        printf("List at position [%d] holds value: %d\n", counter, list->id);
+        counter++;
+        list = list->next;
+    }
 }
--- LinkedList_C/linkedList.h
+++ LinkedList_C/linkedList.h
@@ -1,1 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node{
+    int id; 
+    struct node* next;
+}node;
+
+node * createHeadNode();
+void printList();
+void insertToBack(node * head, int value);
+void insertToFront(node * head, int value);
